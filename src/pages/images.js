725278e7a5de870b8f282afc9a2eba3f8ad65b3f
NidhiPatel <nidhi.patel@alphacertlabs.com>
2020-12-23 20:47:40 +1300

    deploy hello world

---
+++ src/pages/images.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Images = () => (
+  <Layout>
+    <SEO title="Page two" />
+    <h1>Projects</h1>
+
+   
+  </Layout>
+)
+
+export default Images
